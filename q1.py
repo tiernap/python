afea11f8d9a00d095981f0da23f80dd475ec2ad6
peterti <43565301+peterti@users.noreply.github.com>
2018-11-16 11:14:57 +0100

    added q1

---
+++ q1.py
@@ -0,0 +1,19 @@
+print("\t"*2+"Title")
+print("hello")
+print('hello world')
+a = 5
+b = 12.3456789
+c = a+b
+#d="A number "+b
+e = a+ \
+    b+ \
+    c
+f = 2
+g=a/f
+print(a)
+print(b)
+print(c)
+#print(d)
+print(e)
+print(f)
+print(g)
