9d22c94121c10088e2c6a4734b83d0b0422bd725
peterti <43565301+peterti@users.noreply.github.com>
2018-11-16 11:17:53 +0100

    added question2

---
+++ q2.py
@@ -0,0 +1,21 @@
+linelen = 51
+string1 = 'title'
+string2 = 'A Option a'
+string3 = 'b option b'
+string4 = 'c option c'
+print("x"*linelen)
+print("<"+" "*(linelen-2)+">")
+print("<"+" "*(linelen-2)+">")
+print("<"+" "*(linelen-2)+">")
+print("<"+" "*(linelen-2)+">")
+
+
+print("<"+" "*((linelen/2)-(len(string1)/2)-1)+string1+" "*((linelen/2)-(len(string1)/2)-1)+">")
+print("<"+" "*(linelen-2)+">")
+print("<"+" "*(linelen-2)+">")
+print("<"+" "*((linelen/2)-(len(string2)/2))+string2+" "*((linelen/2)-(len(string2)/2))+">")
+print("<"+" "*((linelen/2)-(len(string3)/2))+string3+" "*((linelen/2)-(len(string3)/2))+">")
+print("<"+" "*((linelen/2)-(len(string4)/2))+string4+" "*((linelen/2)-(len(string4)/2))+">")
+print("<"+" "*(linelen-2)+">")
+print("<"+" "*(linelen-2)+">")
+print("x"*linelen)
